40f5412611d0b728470fc8187a76dba89132a729
Wostafa <mostaphaaa@gmail.com>
2022-10-21 18:32:41 +0330

    Cart responsive finished

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -7,7 +7,7 @@ import { selectCart, deleteProduct } from '../Redux/CartSlice';
 import { selectUser } from '../Redux/UserSlice';
 import { useNavigate } from 'react-router-dom';
 import useStripe from './Stripe';
-import { StripeConfig } from '../Constants';
+import { StripeConfig, QUERIES } from '../Constants';
 
 function Cart(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -89,9 +89,11 @@ function Cart(): JSX.Element {
               </ImageWrapper>
               <Name>{pr.name}</Name>
               <Price>${pr.price}</Price>
+              <ButtonWrapper>
               <DeleteButton title='Remove Product' onClick={() => deleteHandler(pr.key)} disabled={isDeleting}>
                 <Trash size={20} color={'#FB2E86'} />
               </DeleteButton>
+              </ButtonWrapper>
             </Product>
           ))}
         </ProductsWrapper>
@@ -118,9 +120,22 @@ function Cart(): JSX.Element {
 const Wrapper = styled.div`
   display: flex;
   gap: 64px;
+
+  @media ${QUERIES.laptopAndSmaller} {
+    gap:16px;
+  }
+  @media ${QUERIES.tabletAndSmaller} {
+    flex-direction: column;
+  }
 `;
 const TotalWrapper = styled.aside`
   flex: 1;
+  max-width: 500px;
+  width: 100%;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    align-self: center;
+  }
 `;
 const TotalBox = styled.div`
   background-color: var(--color-fill-page2);
@@ -155,34 +170,58 @@ const ProductsWrapper = styled.ul`
   flex: 3;
 `;
 const Product = styled.li`
-  display: flex;
-  justify-content: space-between;
-  height: 110px;
-  align-items: center;
-
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
   padding: 8px 16px;
+  gap: 16px;
   border-bottom: 1px solid var(--color-border-gray);
+
+  @media ${QUERIES.phoneAndSmaller} {
+    display:flex;
+    flex-direction:column;
+    gap:16px;
+    align-items:center;
+  }
 `;
 const ImageWrapper = styled.div`
-  height: 100%;
+  height: 180px;
+  width: 180px;
 
   img {
     height: 100%;
+    width: 100%;
+    object-fit: cover;
     display: block;
+    border-radius: var(--image-radius);
   }
 `;
 const Name = styled.h4`
+margin:0;
+display: flex;
+align-items: center;
+justify-content: center;
   color: var(--color-off-blue);
 `;
 const Price = styled.h4`
+margin:0;
+display: flex;
+align-items: center;
+justify-content: center;
   color: var(--color-off-blue);
 `;
-
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const DeleteButton = styled(UnstyledButton)`
+  width:fit-content;
+  height: fit-content;
   display: flex;
   padding: 6px;
   border-radius: var(--button-radius);
   border: 1px solid var(--color-border-gray);
+
   &:hover {
     border-color: var(--color-border-gray-hover);
   }
@@ -190,6 +229,9 @@ const DeleteButton = styled(UnstyledButton)`
 
 const CheckOutButton = styled(Button)`
   background-color: var(--color-button-green);
+  margin: 0 auto;
+  width:100%;
+  max-width: 280px;
 `;
 
 export default Cart;
